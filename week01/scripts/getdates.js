13c9ad0ee80de019938273ef544da49427479fc2
Devine <devinephoebe433@gmail.com>
2026-03-10 16:01:32 +0300

    devine

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,8 @@
+// Get the current year using the Date object
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
+// Output the current year in the footer
+document.getElementById('current-year').textContent = currentYear;
+
+
