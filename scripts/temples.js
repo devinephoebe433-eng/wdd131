fca5c7347d6de74a7501cc3be122b742c72b6562
Devine <devinephoebe433@gmail.com>
2026-03-24 15:20:33 +0300

    devine433

--- scripts/temples.js
+++ scripts/temples.js
@@ -8,3 +8,14 @@ year.textContent = currentYear;
 /* Display the last modified date of the document */
 const lastModified = document.querySelector("#lastModified");
 lastModified.textContent = document.lastModified;
+
+const menuBtn = document.getElementById("menu-btn");
+const navMenu = document.getElementById("nav-menu");
+
+menuBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("hidden");
+});
+
+document.getElementById("year").textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent = document.lastModified;
